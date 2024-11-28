06b5d918c00b0c28d1bf53fe33b1496ae8e63a61
Jarek <jarek@gmail.com>
2024-11-28 16:02:46 +0100

    2.question postup

---
+++ 2.question.sql
@@ -0,0 +1,57 @@
+/* Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+*/
+
+-- Výpočet množství koupeného mléka za rok 2006
+
+SELECT 
+	round(AVG(payroll_per_year)/avg_value) AS number_of_bought_milk_per_year,
+	avg_value,
+	price_category_name ,
+	price_value ,
+	price_unit ,
+	price_year
+FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
+WHERE price_category_name = 'Mléko polotučné pasterované'
+	AND price_year = '2006';
+
+
+-- Výpočet množství koupeného chleba za rok 2006
+
+SELECT 
+	round(AVG(payroll_per_year)/avg_value) AS number_of_bought_bread_per_year,
+	avg_value,
+	price_category_name ,
+	price_value ,
+	price_unit ,
+	price_year
+FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
+WHERE price_category_name = 'Chléb konzumní kmínový'
+	AND price_year = '2006';
+
+
+
+-- Výpočet množství koupeného mléka za rok 2018
+
+SELECT 
+	round(AVG(payroll_per_year)/avg_value) AS number_of_bought_milk_per_year,
+	avg_value,
+	price_category_name ,
+	price_value ,
+	price_unit ,
+	price_year
+FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
+WHERE price_category_name = 'Mléko polotučné pasterované'
+	AND price_year = '2018';
+
+-- Výpočet množství koupeného chleba za rok 2018
+
+SELECT 
+	round(AVG(payroll_per_year)/avg_value) AS number_of_bought_bread_per_year,
+	avg_value,
+	price_category_name ,
+	price_value ,
+	price_unit ,
+	price_year
+FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
+WHERE price_category_name = 'Chléb konzumní kmínový'
+	AND price_year = '2018';
