133c73f59b51c59f7a3a97f817665f82d1a1ecaa
Jarek <jarek@gmail.com>
2024-12-06 10:52:20 +0100

    menší upravy a práce na 4. question

--- 3. question.sql
+++ 3. question.sql
@@ -1,23 +1,11 @@
 /* Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
- * 
  */
 
-
-SELECT
-	avg_price,
-	price_category_name ,
-	price_value ,
-	price_unit ,
-	price_year 
-FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
-WHERE price_category_name = 'Kapr živý'
-GROUP BY price_year ;
-
--- přidání ceny za předchozí rok
--- přídání rozdílu cen oproti předchozímu roku
+-- přidání sloupce s cenou za předchozí rok
+-- přídání sloupce s rozdílem cen oproti předchozímu roku
 -- vytvoření view
 
-CREATE VIEW price_difference_over_time AS 
+CREATE VIEW price_difference_per_category_over_time AS 
 SELECT
 	avg_price,
 	price_category_name ,
@@ -32,8 +20,20 @@ FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
 GROUP BY price_category_name, price_year;
 
 
--- percentuální růst cen v čase
+-- výpočet percentuálních změn cen potravin v čase
+
+WITH cte_avg_percent_increase AS (
+		SELECT 
+			*,
+			round(((avg_price_previous_year-avg_price)/avg_price_previous_year)*100) AS percent_increase
+		FROM price_difference_per_category_over_time AS pdpcot
+		)
 SELECT 
-	*,
-	round(((avg_price_previous_year-avg_price)/avg_price_previous_year)*100) AS percent_increase
-FROM price_difference_over_time AS pdot ;
+	price_category_name,
+	price_value,
+	price_unit,
+	round(avg(percent_increase),1) AS avg_percent_increase_per_category_name 
+FROM cte_avg_percent_increase
+GROUP BY price_category_name
+ORDER BY avg_percent_increase_per_category_name desc;
+
---
+++ 4. question.sql
@@ -0,0 +1,72 @@
+/* Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+ */
+
+-- přidání sloupce s cenou za předchozí rok
+-- přídání sloupce s rozdílem cen oproti předchozímu roku
+-- vytvoření view
+
+CREATE VIEW payroll_difference_over_time AS
+SELECT
+	payroll_per_year,
+	payroll_year,
+	lag(payroll_per_year) OVER (ORDER BY payroll_year) AS payroll_previous_year,
+	payroll_per_year - lag(payroll_per_year) OVER (ORDER BY payroll_year) AS payroll_difference
+FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
+GROUP BY payroll_year;
+
+
+-- výpočet percentuálních změn mezd v čase
+-- vytvoření view
+
+CREATE VIEW payroll_percent_increase_per_year AS
+SELECT 
+	*,
+	round(((payroll_previous_year-payroll_per_year) / payroll_previous_year)*100,1) AS payroll_percent_increase
+FROM payroll_difference_over_time AS pdot;
+
+
+
+
+-- výpočet sumy cen za jednotlivé roky
+-- přidání sloupce s cenou za předchozí rok
+-- přídání sloupce s rozdílem cen oproti předchozímu roku
+-- vytvoření view
+
+CREATE VIEW price_difference_over_time AS
+SELECT
+	sum(avg_price) AS sum_of_avg_price,
+	price_year,
+	LAG(sum(avg_price)) OVER (
+						PARTITION BY price_category_name ORDER BY price_year) AS avg_price_previous_year,
+	sum(avg_price) - lag(sum(avg_price)) OVER (
+						PARTITION BY price_category_name ORDER BY price_year) AS avg_price_difference
+FROM t_jaroslav_cermak_project_sql_primary_final AS tjcpspf
+GROUP BY price_year;
+
+
+-- výpočet percentuálních změn cen potravin v čase
+-- vytvoření view
+
+CREATE VIEW price_percent_increase_per_year AS
+SELECT 
+	*,
+	round(((avg_price_previous_year-sum_of_avg_price)/avg_price_previous_year)*100,1) AS price_percent_increase
+FROM price_difference_over_time AS pdot
+GROUP BY price_year;
+
+
+
+-- join obou views
+
+SELECT
+	price_year,
+	sum_of_avg_price,
+	price_percent_increase,
+	payroll_per_year,
+	payroll_percent_increase
+FROM 
+	price_percent_increase_per_year AS ppipy 
+	JOIN payroll_percent_increase_per_year AS ppipy2
+	ON ppipy2.payroll_year = ppipy.price_year ;
+
+
